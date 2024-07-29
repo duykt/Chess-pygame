de77e3e4b42fa1a8bf5a065e4255813e51da648e
Khavont <duytruongkhang@gmail.com>
2024-07-29 11:38:23 -0400

    first commit

---
+++ chess/code/main.py
@@ -0,0 +1,27 @@
+import pygame
+from settings import *
+
+class Game:
+    def __init__(self):
+        # setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Chess')
+        self.clock = pygame.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # draw
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
