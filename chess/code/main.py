0cf0ad227ce21356fef9ae05b7f26a6734222492
Khavont <duytruongkhang@gmail.com>
2024-07-30 12:59:01 -0400

    pieces are moving. scrapped pieces class

--- chess/code/main.py
+++ chess/code/main.py
@@ -1,7 +1,6 @@
 import pygame
 from settings import *
 from support import  *
-from pieces import *
 
 class Game:
     def __init__(self):
@@ -13,55 +12,49 @@ class Game:
         self.running = True
         self.import_assets()
 
-        # group
-        self.all_sprites = pygame.sprite.Group()
-        self.white_sprites = pygame.sprite.Group()
-        self.black_sprites = pygame.sprite.Group()
+        # game
+        self.white_turn = True
 
         # board
         self.board_image = pygame.image.load(join('..', 'images', 'board.png')).convert_alpha()
         self.board_image = pygame.transform.scale_by(self.board_image, 5)
         self.board_rect = self.board_image.get_frect(topleft=(0,0))
+        self.display_surface.blit(self.board_image, self.board_rect)
 
+        # pieces
+        self.white_pieces = WHITE_PIECES
+        self.black_pieces = BLACK_PIECES
         self.white_position = WHITE_POSITION
         self.black_position = BLACK_POSITION
 
-        self.create_pieces()
+        self.update_board()
 
-        self.get_positions()
-            ## MAKE VAR FOR WHITE PIECES MAYBE DONT USE CONSTANT
-    def create_pieces(self):
-        for i in range(len(WHITE_PIECES)):
-            piece_image = pygame.transform.scale_by(self.white_pieces_sprites[WHITE_PIECES[i]], 5)
-            pos = (self.white_position[i][0], self.white_position[i][1])
-            match WHITE_PIECES[i]:
-                case 'pawn':   Pawn(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
-                case 'knight': Knight(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
-                case 'bishop': Bishop(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
-                case 'rook':   Rook(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
-                case 'queen':  Queen(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
-                case 'king':   King(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.white_sprites))
+    def update_board(self):
+        for i in range(len(self.white_pieces)):
+            image = pygame.transform.scale_by(self.white_surfs[self.white_pieces[i]], 5)
+            rect = image.get_frect(bottomleft=(self.board_rect.bottomleft[0] + self.white_position[i][0] * TILESIZE, self.board_rect.bottomleft[1] - self.white_position[i][1] * TILESIZE))
+            self.display_surface.blit(image, rect)
 
-        for i in range(len(BLACK_PIECES)):
-            piece_image = pygame.transform.scale_by(self.black_pieces_sprites[BLACK_PIECES[i]], 5)
-            pos = (self.black_position[i][0], self.black_position[i][1])
-            match BLACK_PIECES[i]:
-                case 'pawn':   Pawn(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-                case 'knight': Knight(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-                case 'bishop': Bishop(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-                case 'rook':   Rook(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-                case 'queen':  Queen(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-                case 'king':   King(piece_image, pos, self.white_position, self.black_position, (self.all_sprites, self.black_sprites))
-
-    def get_positions(self):
-        self.white_position = [sprite.position for sprite in self.white_sprites]
-        self.black_position = [sprite.position for sprite in self.black_sprites]
-        print(self.white_position)
-        print(self.black_position)
+        for i in range(len(self.black_pieces)):
+            image = pygame.transform.scale_by(self.black_surfs[self.black_pieces[i]], 5)
+            rect = image.get_frect(bottomleft=(self.board_rect.bottomleft[0] + self.black_position[i][0] * TILESIZE, self.board_rect.bottomleft[1] - self.black_position[i][1] * TILESIZE))
+            self.display_surface.blit(image, rect)
 
     def import_assets(self):
-        self.white_pieces_sprites = folder_importer('..', 'images', 'white pieces')
-        self.black_pieces_sprites = folder_importer('..', 'images', 'black pieces')
+        self.white_surfs = folder_importer('..', 'images', 'white pieces')
+        self.black_surfs = folder_importer('..', 'images', 'black pieces')
+
+    def draw_moves(self, moves):
+        for move in moves:
+            pygame.draw.circle(self.display_surface, (130, 151, 105),
+                               ((move[0] * TILESIZE + TILESIZE/2), (self.board_rect.bottomleft[1] - move[1] * TILESIZE - TILESIZE/2)), 10)
+    def pawn_moves(self, pos):
+        possible_moves = []
+        for i in range(1, 3 if pos[1] == 1 else 2):
+            if (pos[0], pos[1] + i) not in self.white_position:
+                possible_moves.append((pos[0], pos[1] + i))
+            else: break
+        return possible_moves
 
     def run(self):
         while self.running:
@@ -69,19 +62,45 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    for sprite in self.all_sprites:
-                        sprite.selected = False
-                        if sprite.rect.collidepoint(mouse_pos):
-                            sprite.selected = True
 
-            # update
-            self.all_sprites.update(dt)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.display_surface.blit(self.board_image, self.board_rect)
+                    mouse_pos = (pygame.mouse.get_pos()[0] // TILESIZE, 7 - pygame.mouse.get_pos()[1] // TILESIZE)
+                    selected_piece = None
+
+                    if self.white_turn:
+                        if mouse_pos in self.white_position:
+                            selected_piece = self.white_pieces[self.white_position.index(mouse_pos)]
+                            piece_pos = mouse_pos
+                            pygame.draw.rect(self.display_surface, (130, 151, 105),(mouse_pos[0] * TILESIZE,self.board_rect.bottomleft[1] - mouse_pos[1] * TILESIZE - TILESIZE, TILESIZE, TILESIZE))
+                    else:
+                        if mouse_pos in self.black_position:
+                            selected_piece = self.black_pieces[self.black_position.index(mouse_pos)]
+                            piece_pos = mouse_pos
+                            pygame.draw.rect(self.display_surface, (130, 151, 105),(mouse_pos[0] * TILESIZE,self.board_rect.bottomleft[1] - mouse_pos[1] * TILESIZE - TILESIZE, TILESIZE, TILESIZE))
+
+                    match selected_piece:
+                        case 'pawn': moves = self.pawn_moves(mouse_pos)
+                        case 'knight': pass
+                        case 'bishop': pass
+                        case 'rook': pass
+                        case 'queen': pass
+                        case 'king': pass
+                    print(moves)
+                    self.draw_moves(moves)
+
+                    for move in moves:
+                        if mouse_pos == move:
+                            if self.white_turn:
+                                self.white_position[self.white_position.index(piece_pos)] = move
+                                self.white_turn = False
+                            else:
+                                self.black_position[self.black_position.index(piece_pos)] = move
+                                self.white_turn = True
+                            pygame.draw.rect(self.display_surface, (130, 151, 105), (move[0] * TILESIZE, self.board_rect.bottomleft[1] - move[1] * TILESIZE - TILESIZE, TILESIZE, TILESIZE))
 
             # draw
-            self.display_surface.blit(self.board_image, self.board_rect)
-            self.all_sprites.draw(self.display_surface)
+            self.update_board()
             pygame.display.update()
 
         pygame.quit()
--- chess/code/pieces.py
+++ chess/code/pieces.py
@@ -1,7 +1,7 @@
 from settings import *
 
 class Piece(pygame.sprite.Sprite):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
         super().__init__(groups)
         self.image = surf
         self.original_image = self.image
@@ -9,57 +9,105 @@ class Piece(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(bottomleft=(BOARD_BOTTOM_LEFT[0] + pos[0] * TILESIZE,
                                                      BOARD_BOTTOM_LEFT[1] - pos[1] * TILESIZE))
         self.selected = False
-        self.possible_moves = []
-        self.white_pos = white_pos
-        self.black_pos = black_pos
+        self.side = side
+
+        if self.side == 'white':
+            self.ally_pos = white_pos
+            self.enemy_pos = black_pos
+        else:
+            self.ally_pos = black_pos
+            self.enemy_pos = white_pos
+
 
     def piece_selected(self):
         if self.selected:
             self.image = pygame.transform.grayscale(self.image)
-            self.moves()
+            self.possible_moves = self.moves()
         else:
             self.image = self.original_image
 
-    def update(self, dt):
+    def update(self, dt, white_pos, black_pos):
         self.piece_selected()
+        if self.side == 'white':
+            self.ally_pos = white_pos
+            self.enemy_pos = black_pos
+        else:
+            self.ally_pos = black_pos
+            self.enemy_pos = white_pos
 
 class Pawn(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
         pass
 
 class Knight(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
         pass
 class Bishop(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
         pass
 
 class Rook(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
-        pass
+        possible_moves = []
+        # calculate movement to the left
+        for row in range(-1, -8, -1):
+            if (self.position[0] + row, self.position[1]) not in self.ally_pos and self.position[0] + row >= 0:
+                possible_moves.append((self.position[0] + row, self.position[1]))
+                if (self.position[0] + row, self.position[1]) in self.enemy_pos:
+                    break
+            else:
+                break
+
+        # movement to the right
+        for row in range(1, 8):
+            if (self.position[0] + row, self.position[1]) not in self.ally_pos and self.position[0] + row < 8:
+                possible_moves.append((self.position[0] + row, self.position[1]))
+                if (self.position[0] + row, self.position[1]) in self.enemy_pos:
+                    break
+            else:
+                break
+
+        # movement up
+        for col in range(1, 8):
+            if (self.position[0], self.position[1] + col) not in self.ally_pos and self.position[1] + col < 8:
+                possible_moves.append((self.position[0], self.position[1] + col))
+                if (self.position[0], self.position[1] + col) in self.enemy_pos:
+                    break
+            else:
+                break
+
+        # movement down
+        for col in range(-1, -8, -1):
+            if (self.position[0], self.position[1] + col) not in self.ally_pos and self.position[1] + col >= 0:
+                possible_moves.append((self.position[0], self.position[1] + col))
+                if (self.position[0], self.position[1] + col) in self.enemy_pos:
+                    break
+            else:
+                break
+        return possible_moves
 
 class Queen(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
         pass
 
 class King(Piece):
-    def __init__(self, surf, pos, white_pos, black_pos, groups):
-        super().__init__(surf, pos, white_pos, black_pos, groups)
+    def __init__(self, surf, pos, white_pos, black_pos, side, groups):
+        super().__init__(surf, pos, white_pos, black_pos, side, groups)
 
     def moves(self):
         pass
